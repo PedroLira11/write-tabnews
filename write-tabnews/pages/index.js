df562d5952b13985c31953e00cda0313f2c89e75
eithan-craig <contatopedroLira10@gmail.com>
2024-07-22 07:55:02 -0300

    do new sentence

--- write-tabnews/pages/index.js
+++ write-tabnews/pages/index.js
@@ -1,5 +1,6 @@
 function Home () {
-    return <h1> Lutar, Lutar! e morrer! </h1>
+    return <h1> Nós vamos Lutar, Lutar! e morrer!. coringa sentence:
+        polvora, dinamite e gasolina, essas coisas são baratas!  </h1>
 }
 
 export default Home;
