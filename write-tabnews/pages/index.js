37cbdf6c40b74aa0c6bd9a91566740ddaeed6af9
israel <contatopedrolira10@gmail.com>
2024-09-06 11:16:31 -0300

    modifica a frase

--- write-tabnews/pages/index.js
+++ write-tabnews/pages/index.js
@@ -2,7 +2,7 @@ function Home () {
     return <h1> Nós vamos Lutar, Lutar! e morrer!.
          coringa sentence: eu gosto de polvora, dinamite e gasolina,
          essas coisas são baratas!
-        . nós vamos até o fim. why you so serius, lets put in smile your face!  </h1>
+        . nós vamos até o fim. why you so serius, lets put in smile your face! :D </h1>
     return <h1>EU sou o Caminho, a verdade e a vida.</h1>
 }
 
